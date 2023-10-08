8db6b41154fed60ea919138a06ceec892543465d
viv3000 <viktor05012005@yandex.ru>
2023-10-08 20:58:00 +0300

    add datetime in logging

--- src/log.py
+++ src/log.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 class Log:
     def log(self, message, error=None):
         log = f'{message}:   {error}\n'
         f = open("log.txt", "a")
-        f.write(log)
+        f.write(str(datetime.now()) + ":" + log)
         f.close()
         print(log)
