d2bb09fe8171bd7bdef5005303ddf05621acdec2
viv3000 <viktor05012005@yandex.ru>
2023-10-08 22:45:51 +0300

    add 'gap' via messages with social network

--- src/handlers/message.py
+++ src/handlers/message.py
@@ -8,10 +8,14 @@ def message_handler(bot):
     def getTextMessages(message):
         Log().log(f'{message.from_user.username}: {message.text}');
         messages = {
-                "Контакты"             : "Школка: https://mkeiit.ru\ngithub: https://github.com/viv3000/TRBFM\npornhub - https://pornhub.com/viv3000\ntg: https://t.me/viv_3000\nvk: https://vk.ru/viv3000",
+                "Контакты"             : "Школка: https://mkeiit.ru\n\ngithub: https://github.com/viv3000/TRBFM\n\npornhub - https://pornhub.com/viv3000\n\ntg: https://t.me/viv_3000\n\nvk: https://vk.ru/viv3000",
                 "Списочек коммандочек" : "Приветульки малышь!!!\n /start - начать общение сначала\n /getTimetable - попросить расписание \n /getCallTimetable - получить расписание звонков \n /getCallTimetableMonday - получить расписание звонков на понедельник \n /getCallTimetableFriday - получить расписание звонков на пятницу \n /getCallTimetableSaturday - получить расписание звонков на субботу",
         }
-        
+        if (message.text == "Контакты"):
+            bot.send_message(message.from_user.id, "Школка: https://mkeiit.ru\n\ngithub: https://github.com/viv3000/TRBFM\n\npornhub - https://pornhub.com/viv3000")
+            bot.send_message(message.from_user.id, "tg: https://t.me/viv_3000")
+            bot.send_message(message.from_user.id, "vk: https://vk.ru/viv3000")
+            return
         try:
             bot.send_message(message.from_user.id, messages[message.text]);
         except KeyError as ex:
