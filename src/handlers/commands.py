1a2481a3f9c6ddd17efc689cd2057b85fbd9627d
viv3000 <viktor05012005@yandex.ru>
2023-10-08 22:14:03 +0300

    add /getCallTimetable* (ok this time)

--- src/handlers/commands.py
+++ src/handlers/commands.py
@@ -16,8 +16,24 @@ class TimetableBtn():
     def to_button(self) -> telebot.types.InlineKeyboardButton:
         message = f'{self.weekdays[self.timetable.weekday]}\n({self.timetable.date})'
         return types.InlineKeyboardButton(text=message, callback_data=self.timetable.href)
-
+    
 def commands_handler(bot):
+    @bot.message_handler(commands = ['start'])
+    def startHandler(message):
+        Log().log(message.from_user.username + ': \start');
+        markup = types.ReplyKeyboardMarkup();
+    
+        btns = {
+            btn("в сайт калледжа хочу ссылочку") : "",
+            btn("в гитхаб хочу ссылочку") : "",
+            btn("в порнхаб хочу ссылочку") : "",
+            btn("списочек коммандочек") : "",
+        }
+        for i in btns: 
+            markup.add(types.InlineKeyboardButton(text=i.text))
+    
+        bot.send_message(message.from_user.id, "Приветульки малышь!!!\n /getTimetable - попросить расписание\n /start - начать общение сначала \n /getCallTimetable - получить расписание звонков \n /getCallTimetableMonday - получить расписание звонков на понедельник \n /getCallTimetableFriday - получить расписание звонков на пятницу \n /getCallTimetableSaturday - получить расписание звонков на субботу", reply_markup = markup);
+
     @bot.message_handler(commands = ['getTimetable'])
     def getTimetableHandler(message):
         Log().log(message.from_user.username + ': \getTimetable');
@@ -35,42 +51,24 @@ def commands_handler(bot):
         for i in btns: markup.add(i.to_button())
 
         bot.send_message(message.from_user.id, "Тебе необходимо выбрать дату, ты справишься?", reply_markup = markup);
-    
-    @bot.message_handler(commands = ['start'])
-    def startHandler(message):
-        Log().log(message.from_user.username + ': \start');
-        markup = types.ReplyKeyboardMarkup();
-    
-        btns = {
-            btn("в сайт калледжа хочу ссылочку") : "",
-            btn("в гитхаб хочу ссылочку") : "",
-            btn("в порнхаб хочу ссылочку") : "",
-            btn("списочек коммандочек") : "",
-        }
-        for i in btns: 
-            markup.add(types.InlineKeyboardButton(text=i.text))
-    
-        bot.send_message(message.from_user.id, "Приветульки малышь!!!\n /getTimetable - попросить расписание\n /start - начать общение сначала \n /getCallTimetable - получить расписание звонков \n /getCallTimetableMonday - получить расписание звонков на понедельник \n /getCallTimetableFriday - получить расписание звонков на пятницу \n /getCallTimetableSaturday - получить расписание звонков на субботу", reply_markup = markup);
 
-def commands_handler(bot):
     @bot.message_handler(commands = ['getCallTimetable'])
     def getCallTimetableHandler(message):
         Log().log(message.from_user.username + ': \getCallTimetable');
         bot.send_document(message.from_user.id, "https://mkeiit.ru/wp-content/uploads/2023/09/Расписание-звонков-основное-.pdf");
-
-def commands_handler(bot):
+    
     @bot.message_handler(commands = ['getCallTimetableMonday'])
-    def getCallTimetableHandler(message):
-        Log().log(message.from_user.username + ': \getCallTimetable');
+    def getCallTimetableMondayHandler(message):
+        Log().log(message.from_user.username + ': \getCallTimetableMonday');
         bot.send_document(message.from_user.id, "https://mkeiit.ru/wp-content/uploads/2023/09/Расписание-звонков-на-понедельник-.pdf");
-def commands_handler(bot):
+    
     @bot.message_handler(commands = ['getCallTimetableFriday'])
-    def getCallTimetableHandler(message):
-        Log().log(message.from_user.username + ': \getCallTimetable');
+    def getCallTimetableFridayHandler(message):
+        Log().log(message.from_user.username + ': \getCallTimetableFriday');
         bot.send_document(message.from_user.id, "https://mkeiit.ru/wp-content/uploads/2023/09/Расписание-звонков-на-пятницу-.pdf");
-def commands_handler(bot):
+    
     @bot.message_handler(commands = ['getCallTimetableSaturday'])
-    def getCallTimetableHandler(message):
-        Log().log(message.from_user.username + ': \getCallTimetable');
+    def getCallTimetableSaturdayHandler(message):
+        Log().log(message.from_user.username + ': \getCallTimetableSaturday');
         bot.send_document(message.from_user.id, "https://mkeiit.ru/wp-content/uploads/2023/09/Расписание-звонков-суббота.pdf");
 
