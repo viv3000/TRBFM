730a3d63f3ebf8ccb32bc6668645f0a77eda3178
viv3000 <viktor05012005@yandex.ru>
2023-10-09 20:09:11 +0300

    add images in local cash for optimization

--- src/templates/ConverterPdfToJpg.py
+++ src/templates/ConverterPdfToJpg.py
@@ -1,13 +1,26 @@
 from pdf2image import convert_from_path
+import os
+import os.path
+
 
 class ConverterPdfToJpg:
     def __init__(self):
         pass
+    
     def convert(self, pdfUrl: str) -> list[str]:
-        pages = convert_from_path(pdfUrl)
         jpgUrls = []
-        for i in range(len(pages)):
-            jpgUrls.append(f'img/{pdfUrl[pdfUrl.rfind("/")+1:]}.page.{i}.jpg');
-            pages[i].save(jpgUrls[i], 'JPEG')
+        if (os.path.isfile(f'img/{pdfUrl[pdfUrl.rfind("/")+1:]}.page.0.jpg')):
+            print("local")
+            jpgUrls.append(f'img/{pdfUrl[pdfUrl.rfind("/")+1:]}.page.0.jpg');
+            i = 1
+            while os.path.isfile(f'img/{pdfUrl[pdfUrl.rfind("/")+1:]}.page.{i}.jpg'):
+                jpgUrls.append(f'img/{pdfUrl[pdfUrl.rfind("/")+1:]}.page.{i}.jpg');
+                i=i+1;
+        else:
+            print("site")
+            pages = convert_from_path(pdfUrl)
+            for i in range(len(pages)):
+                jpgUrls.append(f'img/{pdfUrl[pdfUrl.rfind("/")+1:]}.page.{i}.jpg');
+                pages[i].save(jpgUrls[i], 'JPEG')
         return jpgUrls
 
